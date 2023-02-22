7887e65a4c8cd4a63eaaeb681502e199b1d97ff6
mohammad-shofiullah <shoefieullah@gmail.com>
2023-02-22 16:47:45 +0700

    second done

--- app/Database/Migrations/2023-02-04-095648_Penjualan.php
+++ app/Database/Migrations/2023-02-04-095648_Penjualan.php
@@ -15,7 +15,7 @@ class Penjualan extends Migration
                 'unsigned' => true,
                 'auto_increment' => true,
             ],
-            'id_et' => [
+            'id_pro' => [
                 'type' => 'INT',
                 'constraint' => 20,
                 'unsigned' => true,
@@ -43,7 +43,7 @@ class Penjualan extends Migration
             'updated_at'       => ['type' => 'datetime', 'null' => true],
         ]);
         $this->forge->addKey('id_penj', true);
-        $this->forge->addForeignKey('id_et', 'etalase', 'id_et', 'CASCADE', '');
+        $this->forge->addForeignKey('id_pro', 'etalase', 'id_pro', 'CASCADE', '');
         $this->forge->createTable('penjualan');
     }
 
